23612ca48587f6de3c03736187301331b451b95a
manu-castillo <manuelcastillo94@gmail.com>
2022-04-27 15:20:54 -0300

    starting challenge two

--- test/app.js
+++ test/app.js
@@ -1,1 +1,9 @@
-console.log("Hello world");
+const express = require ('express');
+
+const app = express();
+
+app.listen(3000, () => console.log("Servidor corriendo"));
+
+app.get ('/', function(req, res) { 
+    res.send("Bienvenidos al sitio");
+});
